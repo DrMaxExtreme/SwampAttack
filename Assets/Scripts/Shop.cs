7ab1be6c3b243d2c5b0da029240f5eb1d511ea08
DrMax <tehnolog3ooo@yandex.ru>
2022-11-18 19:35:37 +0300

    Alpha_02.1

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private List<Weapon> _weapons;
+    [SerializeField] private Player _player;
+}
